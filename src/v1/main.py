07b196043779711db6d7930e535625e81c9d7af0
amithadiraju1694 <amith.adiraju@gmail.com>
2022-10-15 14:43:50 -0700

    Added better routing and default mechanisms

--- src/v1/main.py
+++ src/v1/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI, Request,Form
+from fastapi import FastAPI, Request,Form, Response
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -8,6 +8,14 @@ templates = Jinja2Templates(directory="../frontend_templates/")
 
 
 @app.get("/")
-def get_inp_text(request: Request):
-    result = "Type few words in any shloka"
-    return templates.TemplateResponse('home.html', context={'request': request, 'result': result})
+async def get_inp_text( request: Request ):
+    return templates.TemplateResponse('home.html', context={'request': request })
+
+
+@app.post("/")
+async def bizl(request: Request):
+    user_form = await request.form()
+
+    user_typed = user_form.get("vsn_inp")
+
+    return templates.TemplateResponse('default.html', context={'request': request , 'result' : user_typed})
