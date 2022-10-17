2da205ebf09d7bb777bc7094038ae27141e3a05e
amithadiraju1694 <amith.adiraju@gmail.com>
2022-10-16 20:56:21 -0700

    Amend reason: Forgot to push from root folder, which included some file changes.
    Changed structure of project.
    Reason: Making routing and packaging complicated when deployed as docker containers.
    
    Modified folder names to follow standards.
    
    Re-oganized entry point and requirement files to ease dockerization.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from fastapi import FastAPI, Request,Form, Response
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="../frontend_templates/")
+templates = Jinja2Templates(directory="templates")
+templates.env.autoescape = False
 
 
 @app.get("/")
