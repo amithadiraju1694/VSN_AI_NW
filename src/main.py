bc2ddcbcff892fedc1a2cf0c82f32a0370a2b62f
amithadiraju1694 <amith.adiraju@gmail.com>
2022-10-17 22:00:34 -0700

    Added Some static files ( css and images ) to the code base. Added docker compose for quicker developments. Modified templates to accept static files. Modified main file to mount static files into source code.

--- src/main.py
+++ src/main.py
@@ -2,18 +2,21 @@ from typing import Union
 
 from fastapi import FastAPI, Request,Form, Response
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 templates.env.autoescape = False
+app.mount("/static", StaticFiles(directory="static"), name="static")  # define the directory for the static files
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def get_inp_text( request: Request ):
     return templates.TemplateResponse('home.html', context={'request': request })
 
 
-@app.post("/")
+@app.post("/", response_class=HTMLResponse)
 async def bizl(request: Request):
     user_form = await request.form()
 
