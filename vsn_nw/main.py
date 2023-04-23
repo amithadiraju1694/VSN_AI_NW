211095487ca1f5f49a881cd3a2c73ee0d5871369
amithadiraju1694 <amith.adiraju@gmail.com>
2023-04-22 20:57:34 -0700

    This is the best structure for docker-izing fastAPI. Works with both docker containers and docker compose. Use this structure going forward and merge to master.
    Signed-off-by: amithadiraju1694 <amith.adiraju@gmail.com>

---
+++ vsn_nw/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, Request,Form, Response
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+import time
+from vsn_nw.model.pred_nw import predict_next_word
+
+from vsn_nw.helpers.model_helps import load_model, validate_inp_text
+from typing import Union
+
+app = FastAPI()
+saved_model = load_model()
+
+#directory: path inside docker container where these files are to be found
+templates = Jinja2Templates(directory="./vsn_nw/templates")
+templates.env.autoescape = False
+app.mount("/vsn_nw/static", StaticFiles(directory="./vsn_nw/static"),name="static")  # define the directory for the static files
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_inp_text( request: Request ):
+    return templates.TemplateResponse('home.html',
+     context={'request': request })
+
+@app.get("/useapp", response_class=HTMLResponse)
+async def get_usage_page(request: Request):
+    return templates.TemplateResponse( 'app_usage.html',
+    context={'request': request}
+    )
+
+@app.post("/", response_class=HTMLResponse)
+async def bizl(request: Request):
+    """ Entire business logic works from here."""
+    user_form = await request.form()
+
+    user_typed = user_form.get("vsn_inp")
+    
+    st_pred = time.time()
+   
+    shlokas = await validate_inp_text(user_typed)
+
+    print("\n Returned shlokas: ", shlokas)
+
+
+    if len(shlokas) > 0:
+        predictions = await predict_next_word(sentences=shlokas,
+         model = saved_model)
+
+        en_pred = time.time()
+
+        print("\n No. of milli-seconds taken for predictions: ", (en_pred-st_pred)*1000)
+        print("\n Predicted next word:" , predictions)
+    else:
+        predictions = ["NONE"]
+
+    ret_results = dict(  zip(shlokas, predictions) )
+
+    return templates.TemplateResponse('results.html',
+     context={'request': request ,
+     'results' : ret_results
+     })
---
+++ vsn_nw/model/pred_nw.py
@@ -0,0 +1,66 @@
+from typing import List, Union
+
+import numpy as np
+import tensorflow as tf
+from typeguard import typechecked
+
+from vsn_nw.helpers.load_assets import load_dp_meta, load_vocab
+from vsn_nw.helpers.model_helps import encode_data, pad_data
+
+w2i, i2w = load_vocab()
+dp_meta = load_dp_meta()
+
+
+@typechecked
+def pre_process_data(
+    sentences: List[str],
+) -> Union[tf.Tensor, tf.SparseTensor, tf.IndexedSlices]:
+    """Pre-processes data by encoding string to indicies from vocabulary
+    and pads them to a fixed length.
+
+    Args:
+        sentences: List of strings containing input text.
+
+    Returns:
+        padded_docs: tf.Tensor/SparseTensor containing padding indices up-to max-length
+        for each sentence.
+    """
+    encoded_docs = encode_data(sentences, w2i)
+    padded_docs = pad_data(encoded_docs, w2i)
+
+    return padded_docs
+
+
+@typechecked
+async def predict_next_word(sentences: List[str], model):
+    padded_docs = pre_process_data(sentences)
+    _, ncols = padded_docs.shape
+
+    all_unk = np.array([w2i["UNK"]] * ncols, dtype=np.int32)
+
+    predictions = []
+    for doc in padded_docs:
+        if np.array_equal(doc, all_unk):
+            predictions.append([w2i["UNK"]])
+        else:
+            predictions.append(model(np.array(doc).reshape(-1, dp_meta["max_length"])))
+    postpro_predictions = await post_process(predictions)
+
+    return postpro_predictions
+
+
+@typechecked
+async def post_process(logits_tensor: List[Union[List, tf.Tensor]]) -> List[str]:
+
+    word_indices = np.zeros(shape=(len(logits_tensor),))
+    for ind, tensor in enumerate(logits_tensor):
+        if isinstance(tensor, list):
+            word_indices[ind] = tensor[0]
+        else:
+            # TODO: Check for shapes of this single tensor
+            # and validate argmax after that
+            word_indices[ind] = tf.math.argmax(tensor, axis=1).numpy()[0]
+
+    word_preds = [i2w.get(ind, None) for ind in word_indices]
+
+    return word_preds
